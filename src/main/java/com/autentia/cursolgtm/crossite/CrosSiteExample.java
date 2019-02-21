adc40f81a63da5eee15385d96718c49cffb5d29a
urbanojvr <u.villanueva95@gmail.com>
2019-02-21 10:40:49 +0100

    crosssite locked

--- src/main/java/com/autentia/cursolgtm/crossite/CrosSiteExample.java
+++ src/main/java/com/autentia/cursolgtm/crossite/CrosSiteExample.java
@@ -1,19 +1,38 @@
 package com.autentia.cursolgtm.crossite;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 public class CrosSiteExample {
 
-    if (clients.putIfAbsent(clientID, answer) != null) {
+    public void returnDataClient(HttpServletRequest request, HttpServletResponse response) {
+        final String clientId = request.getHeader("clientID");
+        //¿Cómo usar el dato manipulado para cross site?
+    }
+
+
+
+
+
+    if(clients.putIfAbsent(clientID,answer)!=null)
+
+    {
         response.sendError(HttpServletResponse.SC_BAD_REQUEST, "A session for clientID '" + clientID + "' has already been established");
 
-        There are a number of reasons why it may make sense to hide or remove an alert, and a number of ways to go about it.
+        There are a number of reasons why it may make sense to hide or remove an alert, and a number of ways to go
+        about it.
         The result is incorrect
 
-        If this result is a false positive, you can tell us about it so that we can improve the query, and hence the results for everyone. See What if I see a false positive? for more details.
-        I'm not interested in this alert
+        If this result is a false positive, you can tell us about it so that we can improve the query, and hence the
+        results for everyone.See What if I see a false positive ? for more details.
+        I 'm not interested in this alert
 
-        If you want to remove only this alert (and not other similar alerts), you can add the comment lgtm [java/xss] to the end of line 196. See Alert suppression for more details.
-        Cross-site scripting vulnerability due to user-provided value.
-                LOG.warn("A session for clientID '" + clientID + "' has already been established");
+        If you want to remove only this alert(and not other similar alerts), you can add the comment lgtm[java / xss]
+        to the end of line 196. See Alert suppression for more details.
+        Cross - site scripting vulnerability due to user - provided value.
+            LOG.warn("A session for clientID '" + clientID + "' has already been established");
         return null;
+
+    }
 
 }
