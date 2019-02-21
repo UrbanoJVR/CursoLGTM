a6bbe830faa1a7ba87323d81c98d0b12fb868e7f
urbanojvr <u.villanueva95@gmail.com>
2019-02-21 10:52:39 +0100

    error capture main created

--- src/main/java/com/autentia/cursolgtm/crossite/CrosSiteExample.java
+++ src/main/java/com/autentia/cursolgtm/crossite/CrosSiteExample.java
@@ -9,30 +9,5 @@ public class CrosSiteExample {
         final String clientId = request.getHeader("clientID");
         //¿Cómo usar el dato manipulado para cross site?
     }
-
-
-
-
-
-    if(clients.putIfAbsent(clientID,answer)!=null)
-
-    {
-        response.sendError(HttpServletResponse.SC_BAD_REQUEST, "A session for clientID '" + clientID + "' has already been established");
-
-        There are a number of reasons why it may make sense to hide or remove an alert, and a number of ways to go
-        about it.
-        The result is incorrect
-
-        If this result is a false positive, you can tell us about it so that we can improve the query, and hence the
-        results for everyone.See What if I see a false positive ? for more details.
-        I 'm not interested in this alert
-
-        If you want to remove only this alert(and not other similar alerts), you can add the comment lgtm[java / xss]
-        to the end of line 196. See Alert suppression for more details.
-        Cross - site scripting vulnerability due to user - provided value.
-            LOG.warn("A session for clientID '" + clientID + "' has already been established");
-        return null;
-
-    }
-
+    //https://lgtm.com/projects/g/adityasharad/lgtm-workshop/alerts/?mode=list
 }
---
+++ src/main/java/com/autentia/cursolgtm/errorcapture/errorCaptureMain.java
@@ -0,0 +1,7 @@
+package com.autentia.cursolgtm.errorcapture;
+
+public class errorCaptureMain {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
