ad008488318093cff946395d7f8b83fbd9d0766a
urbanojvr <u.villanueva95@gmail.com>
2019-02-22 13:24:31 +0100

    Insecure deserialization vulnerability ended. Gson dependency added to POM

--- src/main/java/com/autentia/cursolgtm/insecureDeserialization/Car.java
+++ src/main/java/com/autentia/cursolgtm/insecureDeserialization/Car.java
@@ -1,8 +1,60 @@
 package com.autentia.cursolgtm.insecureDeserialization;
 
-public class Car {
+import java.io.Serializable;
+
+public class Car implements Serializable {
 
     private String plateNumber;
     private String model;
+    private String ownerName;
+    private int year;
 
+    Car(String plateNumber, String model, String ownerName, int year) {
+        this.plateNumber = plateNumber;
+        this.model = model;
+        this.ownerName = ownerName;
+        this.year = year;
+    }
+
+    public String getPlateNumber() {
+        return plateNumber;
+    }
+
+    public void setPlateNumber(String plateNumber) {
+        this.plateNumber = plateNumber;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getOwnerName() {
+        return ownerName;
+    }
+
+    public void setOwnerName(String ownerName) {
+        this.ownerName = ownerName;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "plateNumber='" + plateNumber + '\'' +
+                ", model='" + model + '\'' +
+                ", ownerName='" + ownerName + '\'' +
+                ", year=" + year +
+                '}';
+    }
 }
--- src/main/java/com/autentia/cursolgtm/insecureDeserialization/deserializationMain.java
+++ src/main/java/com/autentia/cursolgtm/insecureDeserialization/deserializationMain.java
@@ -1,9 +1,69 @@
 package com.autentia.cursolgtm.insecureDeserialization;
 
 import com.autentia.App;
+import com.google.gson.Gson;
+
+import java.io.*;
+import java.util.Scanner;
 
 public class deserializationMain {
+
+    private static Car car = new Car("1234ABC",
+            "Opel Insignia",
+            "Paco Perez Pinto",
+            2013);
+
     public static void main(String[] args) {
-        App.main(args);
+        chooseOption();
+    }
+
+    private static void chooseOption(){
+        System.out.println("What do you want to do?:");
+        System.out.println("1) Save car data");
+        System.out.println("2) Load car data");
+        System.out.println("Enter your option:");
+        String userInput = new Scanner(System.in).nextLine();
+        int userOption = Integer.parseInt(userInput);
+        switch (userOption){
+            case 1:
+                carToJson(car);
+                break;
+            case 2:
+                jsonToCar();
+                break;
+            default:
+                System.err.println("You must enter a valid option!");
+        }
+    }
+
+    private static void carToJson(Car car){
+        try {
+            Gson gson = new Gson();
+            String json = gson.toJson(car);
+            System.out.println(json);
+            BufferedWriter writer = new BufferedWriter(new FileWriter("myCar.txt"));
+            writer.write(json);
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void jsonToCar(){
+        try{
+            Gson gson = new Gson();
+            BufferedReader reader = new BufferedReader(new FileReader("myCar.txt"));
+            String json = reader.readLine();
+            reader.close();
+            Car loadedCar = gson.fromJson(json, Car.class);
+            System.out.println("-------- ORIGINAL CAR DATA: ---------");
+            System.out.println(car.toString());
+            System.out.println("-------- LOADED CAR DATA: ---------");
+            System.out.println(loadedCar.toString());
+        } catch (FileNotFoundException e) {
+            System.err.println("File not found. First, execute option 1 to create the file!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
