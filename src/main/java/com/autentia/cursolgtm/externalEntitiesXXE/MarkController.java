d238939e4f99038cf714a44fa42b2cc62de56099
urbanojvr <u.villanueva95@gmail.com>
2019-02-26 11:51:51 +0100

    MarkController: unused import removed

--- src/main/java/com/autentia/cursolgtm/externalEntitiesXXE/MarkController.java
+++ src/main/java/com/autentia/cursolgtm/externalEntitiesXXE/MarkController.java
@@ -9,7 +9,6 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
-import javax.xml.bind.JAXBException;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
